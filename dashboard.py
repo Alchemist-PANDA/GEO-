649871b9e03ec8da97320c9e4132c6ab4ca9c975
MOHAMMAD Noor <nmlnoor6@gmail.com>
2026-07-15 16:00:44 +0500

    simplify dashboard entry after shared theme integration

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,6 @@
 """BrandSight GEO Streamlit entry point."""
 
 from geo_audit_agent.ui.audit_workspace import render_audit_workspace
-from geo_audit_agent.ui.theme import apply_theme
+
 
 render_audit_workspace()
-apply_theme()
